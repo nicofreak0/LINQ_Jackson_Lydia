eac649785b592a282868fa50d1157a5d1f04650b
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-24 15:05:39 -0500

    l

--- Game.cs
+++ Game.cs
@@ -8,15 +8,19 @@ namespace LINQ_Jackson_Lydia
 {
     internal class Game
     {
-
+        //property to store the title of the game
         public string Title { get; }
 
+        //property to store the age rating of the game
         public char Esrb {  get; }
 
+        //property to store the genre of the game
         public string Genre { get; }
 
+        //constructor to initialize a new game object
         public Game(string titleParam, char esrbParam, string genreParam)
         {
+            // assigns the provided parameters to the properties
             Title = titleParam;
             Esrb = esrbParam;
             Genre = genreParam;
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace LINQ_Jackson_Lydia
     {
         static void Main(string[] args)
         {
+            //Initialize an array of game objects with their titles, ESRB ratings, and genres
             Game[] games = new Game[]
             {
                 new Game("Minecraft", 'E', "Action-Adventure"),
@@ -21,11 +22,45 @@ namespace LINQ_Jackson_Lydia
                 new Game("Badland", 'E', "Action-Adventure"),
             };
 
+            //LINQ to filter games with titles shorter than 9 characters
             var shortGames = from game in games
                              where game.Title.Length < 9
                              select $"Game Title: {game.Title.ToUpper()}";
 
+            //output the filtered dhort game titles
             foreach(var game in shortGames)
+            {
+                Console.WriteLine(game);
+            }
+
+
+            //use LINQ to find the game minecraft and select its details
+            var mineCraft = games.Where(game => game.Title == "Minecraft")
+                .Select(game => $"Title: {game.Title}, ESRB: {game.Esrb}, Genre: {game.Genre}");
+
+            //display the details of minecraft if it exists
+            Console.WriteLine(mineCraft.FirstOrDefault());
+
+            //LINQ to find all games rated T
+            var tRated = from game in games
+                         where game.Esrb == 'T'
+                         select game.Title;
+
+            //output the titles of the T rated games
+            Console.WriteLine("T Rated Games:");
+            foreach(var game in tRated)
+            {
+                Console.WriteLine(game);
+            }
+
+            //LINQ to find all games rated E that are Action games
+            var eRatedAction = from game in games
+                               where game.Esrb == 'E' && game.Genre.Contains("Action")
+                               select game.Title;
+
+            //output the titles of the E rated action game
+            Console.WriteLine("E Rated Action Games:");
+            foreach (var game in eRatedAction)
             {
                 Console.WriteLine(game);
             }
