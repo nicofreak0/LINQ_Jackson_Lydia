d798ed0778be6b669837a839862c39fed8723865
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-24 13:25:03 -0500

    d

--- Game.cs
+++ Game.cs
@@ -8,5 +8,20 @@ namespace LINQ_Jackson_Lydia
 {
     internal class Game
     {
+
+        public string Title { get; }
+
+        public char Esrb {  get; }
+
+        public string Genre { get; }
+
+        public Game(string titleParam, char esrbParam, string genreParam)
+        {
+            Title = titleParam;
+            Esrb = esrbParam;
+            Genre = genreParam;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQ_Jackson_Lydia
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Jackson_Lydia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game[] games = new Game[]
+            {
+                new Game("Minecraft", 'E', "Action-Adventure"),
+                new Game("Cult of the Lamb", 'T', "Action-Adventure"),
+                new Game("The Stanley Parable", 'NR', "First Person Exploration"),
+                new Game("Superliminal", 'E', "Puzzle"),
+                new Game("Gone Home", 'M', "Adventure"),
+                new Game("Portal", 'T', "Action-Adventure"),
+                new Game("Killer Frequency", 'T', "Horror"),
+                new Game("The Last Campfire", 'E', "Puzzle Adventure"),
+                new Game("Metamorphosis", 'T', "Adventure"),
+                new Game("Badland", 'E', "Action-Adventure"),
+            };
+
+            var shortGames = from game in games;
         }
     }
 }
